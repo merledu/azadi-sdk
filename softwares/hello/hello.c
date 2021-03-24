31cee249a7f53101c8cb2416638640eb1ec6fcd6
Usman <usmanzain95@gmail.com>
2021-03-24 13:25:32 +0500

    changed gpio driver

--- softwares/hello/hello.c
+++ softwares/hello/hello.c
@@ -2,17 +2,7 @@
 #include "gpio.h"
 
 int main(){
-    int a = 1;
-    int b = 5;
 
-    int c = a + b;
-
-    // turn ON gpio pin 3
-    if (c>5)
-        write_gpio(3, 1);
-    else 
-    // turn OFF gpio pin 3
-        write_gpio(3, 0);
-    
+    write_gpio(3, 1);
     return 0;
 }
