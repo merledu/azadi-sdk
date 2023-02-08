60b2907b961761b50796fb098f256c163b4e2d4d
Usman <usmanzain95@gmail.com>
2023-02-08 19:50:24 +0500

    fixed compilation errors

--- external/tflm/tensorflow/lite/micro/memory_planner/greedy_memory_planner.h
+++ external/tflm/tensorflow/lite/micro/memory_planner/greedy_memory_planner.h
@@ -107,6 +107,8 @@ class GreedyMemoryPlanner : public MicroMemoryPlanner {
     return per_buffer_size;
   }
 
+  TF_LITE_REMOVE_VIRTUAL_DELETE
+
  private:
   // Whether a buffer is active in a given time range.
   bool DoesEntryOverlapInTime(const ListEntry* entry, const int first_time_used,
@@ -156,8 +158,6 @@ class GreedyMemoryPlanner : public MicroMemoryPlanner {
 
   // Whether buffers have been added since the last plan was calculated.
   bool need_to_calculate_offsets_;
-
-  TF_LITE_REMOVE_VIRTUAL_DELETE
 };
 
 }  // namespace tflite
