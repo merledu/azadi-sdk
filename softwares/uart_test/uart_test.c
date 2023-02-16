0a38858715c7abd6927a15411a8f95a4de4c7943
Usman <usmanzain95@gmail.com>
2023-02-16 22:04:49 +0500

    updated uart test

--- softwares/uart_test/uart_test.c
+++ softwares/uart_test/uart_test.c
@@ -3,17 +3,26 @@
 #include "platform.h"
 
 int main() {
-  uart_t uart0 = {9600, UART0_BASE_ADDRESS};
-  uart_t uart1 = {9600, UART1_BASE_ADDRESS};
+  int count = 0;
   char str[6] = "waleed";
   char recv_str[6];
-  uart_init(&uart0);
-  uart_send(&uart0, str);
+  uart_t uart0, uart1;
 
-  uart_init(&uart1);
-  int count = 0;
+  uart0.baud_rate = 9600;
+  uart0.base_address = UART0_BASE_ADDRESS;
+  uart_t* uart0_ptr = &uart0;
+
+  uart1.baud_rate = 9600;
+  uart1.base_address = UART1_BASE_ADDRESS;
+  uart_t* uart1_ptr = &uart1;
+
+  uart_init(uart1_ptr);
+  uart_send(uart1_ptr, str);
+
+  uart_init(uart0_ptr);
+
   while (count < 6) {
-    recv_str[count++] = uart_recv_byte_polled(&uart1);
+    recv_str[count++] = uart_recv_byte_polled(uart0_ptr);
   }
 
   return 0;
